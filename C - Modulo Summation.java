292f885d94380ea41b47dca863dedb69503aa759
divyababu-137 <bdiv.it2024@rmd.ac.in>
2026-09-12 13:32:54 +0530

    Add files via upload

---
+++ C - Modulo Summation.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum=0;
+		int minus=0;
+		for(int i=0;i<n;i++){
+			int a = sc.nextInt();
+			minus = a-1;
+			sum+=minus;
+			
+		}
+		System.out.println(sum);
+	}
+}
